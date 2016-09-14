6cbd0ce122305c27f9e3116e79b1cda51212f0bc
Vid Vranjek <vidvranjek@gmail.com>
2016-09-14 09:43:32 +0200

    Improving communiation (dirty code)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,9 @@
 #include <sys/time.h>
 #include <QThread>
 #include <QDebug>
+#include <QTime>
 
-//#define DEBUG
+#define DEBUG
 
 // 46.073268
 // 14.509567
@@ -48,14 +49,19 @@ MainWindow::MainWindow(QWidget *parent) :
 
 void MainWindow::main_slot()
 {
-    QString data_in;
-    while(port->size() > 0){
-        data_in = port->read(64);
-        ui->serialIn->insertPlainText(data_in);
-        if (QString::compare(data_in, "Received OK", Qt::CaseInsensitive) == 0){
-            qDebug("Success!!!");
-        }
-    }
+//    QString data_in;
+//    while(port->size() > 0){
+//        data_in = port->read(64);
+//        if (QString::compare(data_in, "Received OK", Qt::CaseInsensitive) == 0){
+//            qDebug("Success!!!");
+//            ui->serialIn->insertPlainText(data_in);
+//        }
+
+//    }
+
+    ui->serialIn->insertPlainText(port->readAll());
+
+
 }
 
 void MainWindow::on_PortRefresh_released()
@@ -113,18 +119,213 @@ void MainWindow::on_sendButton_released()
     std::string data;
     ui->serialIn->clear();
 
+    bool ok = 0;
+    bool error = 0;
+    int lines = 0;
+    QString data_in, serial_text, serial_line;
+    data_in.clear();
+    serial_text.clear();
 
-    data.append("#########");
-    data.append(ui->latEdit->toPlainText().toLatin1());
-    data.append("#");
-    data.append(ui->lonEdit->toPlainText().toLatin1());
-    data.append("#");
-    data.append(ui->radiusEdit->toPlainText().toLatin1());
-    data.append("$");
+    while (!ok){
+        data.append("#########");
+        data.append(ui->latEdit->toPlainText().toLatin1());
+        data.append("#");
+        data.append(ui->lonEdit->toPlainText().toLatin1());
+        data.append("#");
+        data.append(ui->radiusEdit->toPlainText().toLatin1());
+        data.append("$");
 
-    //port->write()
-    port->write(data.c_str());
+        //port->write()
+        port->write(data.c_str());
 
+        QTime timer;
+        timer.start();
+
+//        while (timer.elapsed() < 500){
+//            while(port->size() > 10 && !ok){
+//                data_in = port->readAll();
+//                serial_text.append(data_in);
+//                if (QString::compare(data_in, "Received OK", Qt::CaseInsensitive) == 0){
+//                    qDebug("Success!!!");
+//                    ok = 1;
+//                }
+//            }
+//        }
+
+
+
+
+
+
+//        while (timer.elapsed() < 5000) {
+//            while (port->size() < 10) {}
+
+
+//            QString msg;
+
+//            do {
+//                msg = port->read(1);
+//                serial_line.append(msg);
+//            } while (msg.compare("\n") != 0);
+
+//            qDebug() << "msg:" << serial_line;
+
+//            if (serial_line.contains("Received lat")) {
+//                if (serial_line.contains(ui->latEdit->toPlainText().toLatin1())) {
+//                    lines++;
+//                }
+//                else {
+//                    error = 1;
+//                }
+//            }
+
+//            do {
+//                msg = port->read(1);
+//                serial_line.append(msg);
+//            } while (msg.compare("\n") != 0);
+
+//            if (serial_line.contains("Received lon")) {
+//                if (serial_line.contains(ui->lonEdit->toPlainText().toLatin1())) {
+//                    lines++;
+//                }
+//                else {
+//                    error = 1;
+//                }
+//            }
+
+//            do {
+//                msg = port->read(1);
+//                serial_line.append(msg);
+//            } while (msg.compare("\n") != 0);
+
+
+//            if (serial_line.contains("Received rad")) {
+//                if (serial_line.contains(ui->radiusEdit->toPlainText().toLatin1())) {
+//                    lines++;
+//                }
+//                else {
+//                    error = 1;
+//                }
+//            }
+
+//            if (lines >= 3 && error == 0) {
+//                qDebug() << "Success";
+//                break;
+//            }
+
+//            if (msg)
+
+
+
+//            if (msg.contains("Unrecognised")) {
+//                    qDebug() << "Error";
+//                    serial_text.clear();
+//                    break;
+//            }
+//            else if (msg.contains("Received OK")) {
+//                ok = 1;
+//                qDebug() << "Success";
+//                break;
+//            }
+//        }
+//        ui->serialIn->insertPlainText(serial_text);
+
+//        if (error = 0) break;
+
+
+
+//        bool lat = 0;
+//        bool lon = 0;
+//        bool rad = 0;
+//        QString msg;
+
+//        while (timer.elapsed() < 5000) {
+//            while (port->size() < 6) {}
+
+//            do {
+//                msg = port->read(1);
+//                serial_line.append(msg);
+//            } while (msg.compare("\n") != 0 && timer.elapsed() < 5000);
+
+//            serial_text.append(msg);
+//            qDebug() << "msg:" << msg;
+
+//            if (msg.contains("Unrecognised")) {
+//                qDebug() << "Error";
+//                serial_text.clear();
+//                port->flush();
+//                break;
+//            }
+
+//            else if (msg.contains("Received lat")) {
+//                if (serial_line.contains(ui->latEdit->toPlainText().toLatin1())) {
+//                    qDebug() << "Lat OK";
+//                    lat = 1;
+//                }
+//                else {
+//                    qDebug() << "Error in lat";
+//                    break;
+//                }
+//            }
+
+//            else if (msg.contains("Received lon")) {
+//                if (serial_line.contains(ui->lonEdit->toPlainText().toLatin1())) {
+//                    qDebug() << "Lon OK";
+//                    lon = 1;
+//                }
+//                else {
+//                    qDebug() << "Error in lon";
+//                    break;
+//                }
+//            }
+
+//            else if (msg.contains("Received rad")) {
+//                if (serial_line.contains(ui->radiusEdit->toPlainText().toLatin1())) {
+//                    qDebug() << "Rad OK";
+//                    rad = 1;
+//                }
+//                else {
+//                    qDebug() << "Error in rad";
+//                    break;
+//                }
+//            }
+//            else if (msg.contains("Received OK") && lat && lon && rad) {
+//                ok = 1;
+//                qDebug() << "Success";
+//                ui->serialIn->insertPlainText(serial_text);
+//                break;
+//            }
+//        }
+
+
+
+
+
+
+
+
+/* to dela */
+        while (timer.elapsed() < 5000) {
+            while (port->size() < 10) {}
+            QString msg = port->readLine();
+            serial_text.append(msg);
+            qDebug() << "msg:" << msg;
+            if (msg.contains("Unrecognised")) {
+                    qDebug() << "Error";
+                    serial_text.clear();
+                    port->flush();
+                    break;
+            }
+            else if (msg.contains("Received OK")) {
+                ok = 1;
+                qDebug() << "Success";
+                ui->serialIn->insertPlainText(serial_text);
+                break;
+            }
+        }
+
+
+    }
 }
 
 void MainWindow::on_pushButton_released()
